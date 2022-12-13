d0b2f16d668379b824348f30666383cd7a9541a9
radit <radityaa.pr@gmail.com>
2022-12-13 09:06:02 +0700

    Update pararel_sum.py

--- pararel_sum.py
+++ pararel_sum.py
@@ -6,13 +6,20 @@ class jumlah(threading.Thread):
         threading.Thread.__init__(self)
         self.awal=awal
         self.akhir=akhir
+
     def run(self):
         self.hasil=0
         for suffix in range (self.awal, self.akhir+1):
-            self.hasil = self.hasil + suffix
+            self.hasil = self.hasil + self.suffix
+            print(self.suffix, "=", self.hasil)
 
-        def hasil(self):
-            return self.hasil
+    def hasil(self):
+        return self.hasil
 
 start = time.time
+
+hitung=jumlah(1, 5000000)
+print(hitung1.awal)
+print(hitung1.akhir)
+end = time.time()
 print(end-start)
